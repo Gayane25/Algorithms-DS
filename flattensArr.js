a8821cd24df827faee475deb1fc9ce7ce6ea49ee
Gayane25 <gayabennu@gmail.com>
2022-02-17 23:07:30 +0400

    feat:added flattens function solved with recursion

--- flattensArr.js
+++ flattensArr.js
@@ -1,4 +1,17 @@
 // Given an array of nested arrays. Write a recursive function that flattens it. (Hint create
 // function that concats arrays).
-
 'use strict';
+
+function flattens(arr) {
+  const flattenedArr = [];
+  arr.forEach(item => {
+    if (Array.isArray(item)) {
+      flattenedArr.push(...flattens(item));
+    } else {
+      flattenedArr.push(item);
+    }
+  });
+  return flattenedArr;
+}
+//console.log(flattens([1, [3, 4, [1, 2]], 10]));
+//console.log(flattens([14, [1, [[[3, []]], 1], 0]]));
