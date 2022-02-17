dae6d673b47ab67266d88654529c1a637674d81c
Gayane25 <gayabennu@gmail.com>
2022-02-17 21:46:26 +0400

    feat:solved with recursion

--- ifOddTrue.js
+++ ifOddTrue.js
@@ -1,2 +1,17 @@
 // Write a recursive function to determine whether all digits of the number are odd or not.
 'use strict';
+
+function ifOddTrue(num) {
+  if (num <= 9 && num % 2) {
+    return true;
+  }
+  let remainder = num % 10;
+  if (remainder % 2 === 0) {
+    return false;
+  }
+  num = Math.trunc(num / 10);
+  return ifOddTrue(num);
+}
+// console.log(ifOddTrue(4211133)); output - false;
+// console.log(ifOddTrue(7791)); output - true;
+// console.log(ifOddTrue(5)); output - true;
